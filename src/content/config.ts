f3da3d97a10802e725aa55815892429615b6597e
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-18 07:39:14 -0800

    start of rebuild...

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { blogSchema } from 'starlight-blog/schema'
 
-const releases = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			versionNumber: z.string(),
-			image: z.object({
-				src: image(),
-				alt: z.string(),
-			}).optional(),
-			date: z.date({ coerce: true }),
-		}),
-});
-
-const archivedreleases = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			versionNumber: z.string(),
-			image: z.object({
-				src: image(),
-				alt: z.string(),
-			}).optional(),
-			date: z.date({ coerce: true }),
-		}),
-});
-
-export const collections = { 
-	docs: defineCollection({ schema: docsSchema() }),
-	releases, archivedreleases, 
+export const collections = {
+	docs: defineCollection({ schema: docsSchema({ extend: blogSchema() }) }),
 };
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+
+// Generated color palettes
+const accent = { 200: '#e9b4e6', 600: '#af00b0', 900: '#530954', 950: '#390f39' };
+const gray = { 100: '#f2f6ff', 200: '#e6eeff', 300: '#b5c2de', 400: '#748abf', 500: '#425687', 700: '#243563', 800: '#13234f', 900: '#0f172b' };
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: { accent, gray },
+		},
+	},
+	plugins: [starlightPlugin()],
+};
